0cdbe361b65e2544ee17a529ad239007b381f3b6
Aykut Kardaş <aykutkrds@gmail.com>
2025-01-31 11:18:33 +0300

    fix: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,6 @@ import Footer from "@/components/landing-footer";
 export default function IndexPage() {
   return (
     <div className="min-h-screen bg-black text-white">
-      <div
-        className="fixed inset-0 bg-[url('https://sjc.microlink.io/ZtwmdFiEUf0_DO7Y4PqPMHLsfJehzZnKChWZXFdvLVTUOIhG0mFCbd6vijSBq24DAYHXIQfgb8N5c8V2kSN2bQ.jpeg')] opacity-10"
-        aria-hidden="true"
-      />
       <div className="relative">
         <Header />
         <main>
